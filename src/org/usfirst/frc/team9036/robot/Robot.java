3f5d98b75dd91a16faeed6ccd18b7d13bb7e8bda
SkyZH <iSkyZH@gmail.com>
2016-07-06 22:44:54 +0800

    accomplish training 1.3

--- src/org/usfirst/frc/team9036/robot/Robot.java
+++ src/org/usfirst/frc/team9036/robot/Robot.java
@@ -12,21 +12,26 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot {
-    Servo servo;
+    Joystick mainJoystick;
+    RobotDrive myRobot;
     
     public Robot() {
-    	servo = new Servo(1);
+    	mainJoystick = new Joystick(1);
+        myRobot = new RobotDrive(new VictorSP(1), new VictorSP(3), new VictorSP(2), new VictorSP(4));
+        myRobot.setExpiration(0.1);
     }
     
     public void robotInit() {
     }
     
     public void autonomous() {
-    	servo.setAngle(180);
     }
     
     public void operatorControl() {
+    	myRobot.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) {
+        	myRobot.arcadeDrive(mainJoystick);
+        	Timer.delay(0.02);
         }
     }
     
