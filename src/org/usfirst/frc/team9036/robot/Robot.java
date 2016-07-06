33c5c446b4cd945ebc364a0247c3da771ba74908
SkyZH <iSkyZH@gmail.com>
2016-07-06 21:44:01 +0800

    add training 1.2

--- src/org/usfirst/frc/team9036/robot/Robot.java
+++ src/org/usfirst/frc/team9036/robot/Robot.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team9036.robot;
 
 
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
@@ -11,39 +12,24 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot {
-    RobotDrive myRobot;
+    Servo servo;
     
     public Robot() {
-        myRobot = new RobotDrive(new VictorSP(1), new VictorSP(3), new VictorSP(2), new VictorSP(4));
-        myRobot.setExpiration(0.1);
+    	servo = new Servo(1);
     }
     
     public void robotInit() {
     }
     
     public void autonomous() {
-        myRobot.setSafetyEnabled(false);
-        myRobot.drive(1.0, 0);
-        Timer.delay(2);
-        myRobot.drive(0, 0);
-        Timer.delay(2);
-        myRobot.drive(1.0, 0);
-        Timer.delay(2);
-        myRobot.drive(0, 0);
+    	servo.setAngle(180);
     }
-
-    /**
-     * Runs the motors with arcade steering.
-     */
+    
     public void operatorControl() {
-        myRobot.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) {
         }
     }
-
-    /**
-     * Runs during test mode
-     */
+    
     public void test() {
     }
 }
